1c8af7a9261630cd8b4580aa238712db3268362c
Nate Jones <nate@endot.org>
2021-10-06 20:08:18 -0700

    Implement 5 functions from archiver
    * archive
    * unarchive
    * extract
    * compress-file
    * decompress-file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/babashka/pod-babashka-fswatcher/babashka"
@@ -8,19 +9,78 @@ import (
 )
 
 func ProcessMessage(message *babashka.Message) (interface{}, error) {
-	fmt.Println("Here")
-	return nil, nil
-}
+	switch message.Op {
+	case "describe":
+		return &babashka.DescribeResponse{
+			Format: "json",
+			Namespaces: []babashka.Namespace{
+				{
+					Name: "pod.archiver",
+					Vars: []babashka.Var{
+						{Name: "archive"},
+						{Name: "unarchive"},
+						{Name: "extract"},
+						{Name: "compress-file"},
+						{Name: "decompress-file"},
+					},
+				},
+			},
+		}, nil
+	case "invoke":
+		switch message.Var {
+		case "pod.archiver/archive":
+			args := []json.RawMessage{}
+			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
+				return nil, err
+			}
 
-func unarchive() {
-	archiver.Unarchive("test.tar.gz", "test")
+			var sources []string
+			if err := json.Unmarshal([]byte(args[0]), &sources); err != nil {
+				return nil, err
+			}
+			var dest string
+			if err := json.Unmarshal([]byte(args[1]), &dest); err != nil {
+				return nil, err
+			}
+
+			return true, archiver.Archive(sources, dest)
+		case "pod.archiver/unarchive":
+			var args []string
+			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
+				return nil, err
+			}
+
+			return true, archiver.Unarchive(args[0], args[1])
+		case "pod.archiver/extract":
+			var args []string
+			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
+				return nil, err
+			}
+
+			return true, archiver.Extract(args[0], args[1], args[2])
+		case "pod.archiver/compress-file":
+			var args []string
+			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
+				return nil, err
+			}
+
+			return true, archiver.CompressFile(args[0], args[1])
+		case "pod.archiver/decompress-file":
+			var args []string
+			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
+				return nil, err
+			}
+
+			return true, archiver.DecompressFile(args[0], args[1])
+		default:
+			return nil, fmt.Errorf("Unknown var %s", message.Var)
+		}
+	default:
+		return nil, fmt.Errorf("Unknown op %s", message.Op)
+	}
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-}
-
-func foo() {
 	for {
 		message, err := babashka.ReadMessage()
 		if err != nil {
