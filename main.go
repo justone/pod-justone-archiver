87064c4caebeeae636cb799f0c500abf404d46b0
Nate Jones <nate@endot.org>
2021-10-25 09:40:19 -0700

    Support compression level and overwrite options

--- main.go
+++ main.go
@@ -9,23 +9,63 @@ import (
 )
 
 type opts struct {
-	OverwriteExisting bool `json:"overwrite-existing"`
+	OverwriteExisting *bool `json:"overwrite-existing"`
+	CompressionLevel  *int  `json:"compression-level"`
 }
 
-var defaultOpts = opts{true}
+var trueValue = true
+var defaultOpts = opts{
+	OverwriteExisting: &trueValue,
+	CompressionLevel:  nil,
+}
 
-func setOverwrite(a interface{}, overwriteExisting bool) {
-	switch v := a.(type) {
-	case *archiver.TarGz:
-		v.OverwriteExisting = overwriteExisting
-	case *archiver.Zip:
-		v.OverwriteExisting = overwriteExisting
-	case *archiver.FileCompressor:
-		v.OverwriteExisting = overwriteExisting
+func setOverwrite(a interface{}, overwriteExisting *bool) {
+	if overwriteExisting != nil {
+		switch v := a.(type) {
+		case *archiver.FileCompressor:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.Rar:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.Tar:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.TarBz2:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.TarGz:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.TarLz4:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.TarSz:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.TarXz:
+			v.OverwriteExisting = *overwriteExisting
+		case *archiver.Zip:
+			v.OverwriteExisting = *overwriteExisting
+		}
 	}
 }
 
-func ProcessMessage(message *babashka.Message) (interface{}, error) {
+func setCompressionLevel(a interface{}, compressionLevel *int) {
+	if compressionLevel != nil {
+		switch v := a.(type) {
+		case *archiver.Bz2:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.Gz:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.Lz4:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.TarBz2:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.TarGz:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.TarLz4:
+			v.CompressionLevel = *compressionLevel
+		case *archiver.Zip:
+			v.CompressionLevel = *compressionLevel
+		}
+	}
+}
+
+func processMessage(message *babashka.Message) (interface{}, error) {
 	switch message.Op {
 	case "describe":
 		return &babashka.DescribeResponse{
@@ -60,7 +100,22 @@ func ProcessMessage(message *babashka.Message) (interface{}, error) {
 				return nil, err
 			}
 
-			return true, archiver.Archive(sources, dest)
+			o := defaultOpts
+			if len(args) == 3 {
+				if err := json.Unmarshal([]byte(args[2]), &o); err != nil {
+					return nil, err
+				}
+			}
+
+			a, err := archiver.ByExtension(dest)
+			if err != nil {
+				return nil, err
+			}
+
+			setOverwrite(a, o.OverwriteExisting)
+			setCompressionLevel(a, o.CompressionLevel)
+
+			return true, a.(archiver.Archiver).Archive(sources, dest)
 		case "pod.archiver/unarchive":
 			args := []json.RawMessage{}
 			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
@@ -83,34 +138,85 @@ func ProcessMessage(message *babashka.Message) (interface{}, error) {
 					return nil, err
 				}
 			}
-			// fmt.Fprintf(os.Stderr, "%+v\n", o)
 
 			a, err := archiver.ByExtension(src)
 			if err != nil {
 				return nil, err
 			}
-			// fmt.Fprintf(os.Stderr, "%+v\n", a.(*archiver.TarGz).OverwriteExisting)
-			// a.(*archiver.TarGz).OverwriteExisting = true
-			// fmt.Fprintf(os.Stderr, "%+v\n", a.(*archiver.TarGz).OverwriteExisting)
-			// fmt.Fprintf(os.Stderr, "%+v\n", a.(*archiver.TarGz))
 			setOverwrite(a, o.OverwriteExisting)
-			// fmt.Fprintf(os.Stderr, "%+v\n", a.(*archiver.TarGz).OverwriteExisting)
 
 			return true, a.(archiver.Unarchiver).Unarchive(src, dest)
 		case "pod.archiver/extract":
-			var args []string
+			args := []json.RawMessage{}
 			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
 				return nil, err
 			}
 
-			return true, archiver.Extract(args[0], args[1], args[2])
+			var src string
+			if err := json.Unmarshal([]byte(args[0]), &src); err != nil {
+				return nil, err
+			}
+
+			var file string
+			if err := json.Unmarshal([]byte(args[1]), &file); err != nil {
+				return nil, err
+			}
+
+			var dest string
+			if err := json.Unmarshal([]byte(args[2]), &dest); err != nil {
+				return nil, err
+			}
+
+			o := defaultOpts
+			if len(args) == 4 {
+				if err := json.Unmarshal([]byte(args[3]), &o); err != nil {
+					return nil, err
+				}
+			}
+
+			a, err := archiver.ByExtension(src)
+			if err != nil {
+				return nil, err
+			}
+			setOverwrite(a, o.OverwriteExisting)
+
+			return true, a.(archiver.Extractor).Extract(src, file, dest)
 		case "pod.archiver/compress-file":
-			var args []string
+			args := []json.RawMessage{}
 			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
 				return nil, err
 			}
 
-			return true, archiver.CompressFile(args[0], args[1])
+			var src string
+			if err := json.Unmarshal([]byte(args[0]), &src); err != nil {
+				return nil, err
+			}
+
+			var dest string
+			if err := json.Unmarshal([]byte(args[1]), &dest); err != nil {
+				return nil, err
+			}
+
+			o := defaultOpts
+			if len(args) == 3 {
+				if err := json.Unmarshal([]byte(args[2]), &o); err != nil {
+					return nil, err
+				}
+			}
+
+			a, err := archiver.ByExtension(dest)
+			if err != nil {
+				return nil, err
+			}
+
+			c := &archiver.FileCompressor{
+				Compressor: a.(archiver.Compressor),
+			}
+
+			setCompressionLevel(a, o.CompressionLevel)
+			setOverwrite(c, o.OverwriteExisting)
+
+			return true, c.CompressFile(src, dest)
 		case "pod.archiver/decompress-file":
 			args := []json.RawMessage{}
 			if err := json.Unmarshal([]byte(message.Args), &args); err != nil {
@@ -138,14 +244,12 @@ func ProcessMessage(message *babashka.Message) (interface{}, error) {
 			if err != nil {
 				return nil, err
 			}
-			// fmt.Fprintf(os.Stderr, "%+v\n", a.OverwriteExisting)
-			c := &archiver.FileCompressor{Compressor: a.(archiver.Compressor), Decompressor: a.(archiver.Decompressor)}
+			c := &archiver.FileCompressor{
+				Compressor:   a.(archiver.Compressor),
+				Decompressor: a.(archiver.Decompressor),
+			}
 
 			setOverwrite(c, o.OverwriteExisting)
-			// fmt.Fprintf(os.Stderr, "%+v\n", o.OverwriteExisting)
-			// fmt.Fprintf(os.Stderr, "%+v\n", reflect.TypeOf(o))
-			// fmt.Fprintf(os.Stderr, "%+v\n", c.OverwriteExisting)
-			// fmt.Fprintf(os.Stderr, "%+v\n", reflect.TypeOf(c))
 
 			return true, c.DecompressFile(src, dest)
 		default:
@@ -164,7 +268,7 @@ func main() {
 			continue
 		}
 
-		res, err := ProcessMessage(message)
+		res, err := processMessage(message)
 		if err != nil {
 			babashka.WriteErrorResponse(message, err)
 			continue
